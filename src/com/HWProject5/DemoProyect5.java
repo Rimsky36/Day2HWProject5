0fc221e650a2a65423c1abcdd5984f0bdd13f5a9
Rimsky <Rimsky@192.168.1.145>
2023-04-09 17:05:28 -0700

    commit add() method

--- src/com/HWProject5/DemoProyect5.java
+++ src/com/HWProject5/DemoProyect5.java
@@ -5,6 +5,13 @@ public class DemoProyect5 {
 	public static void main(String[] args) {
 
 		System.out.println("New test project5");
+		System.out.println("New test project5");
 
 	}
-}
+	
+	public void add(int a, int b) {
+		
+		System.out.println(a + b);
+
+	}
+	}
